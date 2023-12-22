f0653a09d3dc83c0f072e7aac9eda71b7d5d0536
Райчо <raioosered@gmail.com>
2023-12-22 16:58:07 +0200

    update loader and readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-Read more: https://packagist.org/packages/rkstylex/packages
+Read more: https://packagist.org/packages/rkstylex/packages <br>
 Dev install: ```composer require rkstylex/packages:dev-dev```
+<br>Add in config/app.php <br>
+providers [<br>
+\Rkstylex\Services\Loader::class <br>
+]
--- src/services/Loader.php
+++ src/services/Loader.php
@@ -3,14 +3,34 @@ namespace Rkstylex\Services;
 use Illuminate\Support\ServiceProvider;
 class Loader extends ServiceProvider
 {
-    public function __construct()
-    {
-            echo 'class test';
-    }
-
+    /**
+     * @return void
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
     public function register()
     {
-        echo dd(base_path());
+        // Check if the base directory bundles exists
+        $dir = base_path('bundles');
+        !is_dir($dir) ? mkdir($dir) : null;
+        array_filter(scandir($dir), function ($item) use ($dir) {
+            if( !in_array($item, ['.', '..'])) {
+                $bundleDirectory = $dir.'\\'.$item;
+                is_dir($bundleDirectory) ? $this->loadBundle($bundleDirectory)  : null;
+            }
+        });
+
+    }
+
+    /**
+     * @param $bundleDirectory
+     * @return void
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    private function loadBundle($bundleDirectory): void
+    {
+        $initFile = $bundleDirectory.'\\init.php';
+        is_file($initFile) ? require_once($initFile): throw new \Illuminate\Contracts\Filesystem\FileNotFoundException($initFile .' | file not found');
     }
 }
+
 new Loader();
